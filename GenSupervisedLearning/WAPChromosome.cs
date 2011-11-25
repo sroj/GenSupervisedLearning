9e4705e4ceed39b094be29e3dce79358128353ea
maxrevilo <oliver.a.perez.c@gmail.com>
2011-11-24 21:32:26 -0430

    Primer Commit de Oliver

---
+++ GenSupervisedLearning/WAPChromosome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Genetic;
+
+
+namespace GenSupervisedLearning
+{
+    public class WAPChromosome : BinaryChromosome
+    {
+        public WAPChromosome()
+            : base(8)
+        {
+
+        }
+    }
+}
